282c87ff18096b20c7a440607ff515d580a88cdd
Padunk <abraham.agel@gmail.com>
2020-10-20 08:49:07 +0700

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+    return (
+        <div className="bg-gray-900">
+            <p className="text-white">Hello World</p>
+        </div>
+    );
+}
+
+export default App;
