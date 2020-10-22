f5ae2ec5a6bfff506c454a229baa1fceeaddbe55
Padunk <abraham.agel@gmail.com>
2020-10-22 08:20:02 +0700

    add styling

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -9,17 +9,27 @@ const Hero = () => {
                 backgroundImage: `url(${background})`,
             }}
         >
+            <div
+                style={{
+                    backdropFilter: "blur(1.5px)",
+                    width: "100%",
+                    height: "100%",
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                }}
+            />
             <svg
                 preserveAspectRatio="none"
                 viewBox="0 0 100 100"
-                className="absolute right-0 min-h-full w-32 -mr-16 hidden lg:block"
+                className="absolute right-0 min-h-full w-32 -mr-16 hidden lg:block z-10"
                 style={{ fill: "#1a202c" }}
             >
                 <polygon points="0,0 50,0 100,100 50,100" />
             </svg>
             <div
-                className="pl-4 pr-4 pt-8 text-orange-200 w-full lg:pl-8"
-                style={{ backgroundColor: "rgba(0,0,0, 0.6)" }}
+                className="pl-4 pr-4 pt-8 text-orange-200 w-full lg:pl-8 relative"
+                style={{ backgroundColor: "rgba(0,0,0, 0.4)" }}
             >
                 <h1 className="font-bold text-4xl lg:text-6xl text-orange-400">
                     Our Stories
