c75c8230baee477389db0a580a221abcbbadbc0f
Padunk <abraham.agel@gmail.com>
2020-10-23 17:39:26 +0700

    add private route and 404

--- client/src/Router/Router.tsx
+++ client/src/Router/Router.tsx
@@ -1,20 +1,53 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import NotFound from "../Pages/404";
 import About from "../Pages/About";
+import Feed from "../Pages/Feed";
 import Home from "../Pages/Home";
 
-const Router = () => {
+function PrivateRoute({ children, ...rest }: any) {
+    return (
+        <Route
+            {...rest}
+            render={({ location }) =>
+                rest.authenticated ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/",
+                            state: { from: location },
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+const Router = ({ authenticated }: any) => {
     return (
         <BrowserRouter>
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/about" exact component={About} />
+                    <PrivateRoute path="/feed" authenticated={authenticated}>
+                        <Feed />
+                    </PrivateRoute>
+                    <Route path="*" component={NotFound} />
                 </Switch>
             </Layout>
         </BrowserRouter>
     );
 };
 
-export default Router;
+const mapStateToProps = ({ user }: any) => {
+    return {
+        authenticated: user.authenticated,
+    };
+};
+
+export default connect(mapStateToProps)(Router);
