963215cb581d1fe86796a8e3a2a642f8081dfd19
Padunk <abraham.agel@gmail.com>
2020-10-31 17:49:02 +0700

    styling...

--- client/src/Pages/404.tsx
+++ client/src/Pages/404.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../Auth/components/Container/Container";
 
 const NotFound = () => {
-    return <div>Not Found</div>;
+    return (
+        <Container>
+            <div className="text-center text-orange-500">
+                <h2 className="text-4xl">Not Found</h2>
+                <p>The page you are looking for doesn't exist.</p>
+                <p>Error code: 404</p>
+                <Link to="/">Back to Home</Link>
+            </div>
+        </Container>
+    );
 };
 
 export default NotFound;
