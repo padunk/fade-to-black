045ecad931bfb5a431a47cbb38cf221ae1c80d61
Padunk <abraham.agel@gmail.com>
2020-11-04 12:08:04 +0700

    user components

---
+++ client/src/Pages/UserDetail.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { axios } from "../Axios";
+import ProfileCard from "../components/ProfileCard/ProfileCard";
+import WhispersList from "../components/WhispersList/WhispersList";
+
+const UserDetail = () => {
+    const params = useParams<{ userName: string }>();
+    const [user, setUser] = React.useState<any | null>(null);
+    const [fetchStatus, setFetchStatus] = React.useState<
+        "pending" | "success" | "fail"
+    >("pending");
+    const [errorMessage, setErrorMessage] = React.useState<string>("");
+
+    React.useEffect(() => {
+        const getUserDetails = async (userName: any) => {
+            setFetchStatus("pending");
+            try {
+                const response = await axios.get(`/user/${userName}`);
+                const data = await response;
+
+                setUser(data.data);
+                setFetchStatus("success");
+            } catch (error) {
+                console.log("error", error);
+                setErrorMessage(error.message);
+                setFetchStatus("fail");
+            }
+        };
+
+        getUserDetails(params.userName);
+    }, []);
+    return (
+        <div className="relative">
+            {fetchStatus === "pending" ? (
+                <div>Loading</div>
+            ) : fetchStatus === "fail" ? (
+                <div>{errorMessage}</div>
+            ) : (
+                <>
+                    <ProfileCard user={user} />
+                    <WhispersList whispers={user?.whispers} />
+                </>
+            )}
+        </div>
+    );
+};
+
+export default UserDetail;
--- client/src/Pages/UserProfile.tsx
+++ client/src/Pages/UserProfile.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import { RootState } from "types";
 import { BsChevronLeft } from "react-icons/bs";
-import { FiEdit } from "react-icons/fi";
 import { useHistory } from "react-router-dom";
 import Button from "../components/Button/Button";
 import EditProfile from "../components/Modal/EditProfile";
 import ChangeImageProfile from "../components/Modal/ChangeImageProfile";
+import ProfileCard from "../components/ProfileCard/ProfileCard";
 
 type IUserProfileProps = ReturnType<typeof mapStateToProps>;
 
 const UserProfile: React.FC<IUserProfileProps> = ({ user }) => {
     const history = useHistory();
-    const [modalStatus, setModalStatus] = React.useState<boolean>(false);
-    const [updateImageModal, setUpdateImageModal] = React.useState<boolean>(
-        false
-    );
+    const [editModal, updateEditModal] = React.useState<boolean>(false);
+    const [imageModal, updateImageModal] = React.useState<boolean>(false);
 
     return (
         <div className="relative">
@@ -28,65 +26,23 @@ const UserProfile: React.FC<IUserProfileProps> = ({ user }) => {
                     <span>Back</span>
                 </div>
             </header>
-            <div className="flex flex-col items-center p-6 mb-6 justify-center shadow-lg bg-gray-800 rounded-md mt-2 mx-auto max-w-lg">
-                <div className="border-b border-purple-800 w-full mb-8 pb-2 text-orange-500 stroke-current">
-                    <div className="flex flex-col items-center">
-                        <div className="rounded-full w-32 h-32 overflow-hidden border-4 border-purple-300 mx-auto">
-                            {/* <img src={user?.imageURL} alt={user?.userName} /> */}
-                            <img
-                                src={require("../assets/images/user1.jpg")}
-                                alt={user?.userName}
-                                className="object-cover border-2 border-transparent rounded-full"
-                            />
-                        </div>
-                        <FiEdit
-                            className="stroke-current ml-32 -mt-3 transform hover:scale-110 transition-transform duration-300 cursor-pointer"
-                            onClick={() =>
-                                setUpdateImageModal(!updateImageModal)
-                            }
-                        />
-                    </div>
-                    <h2 className="text-opacity-75 text-gray-400 text-center">
-                        @{user?.userName}
-                    </h2>
-                </div>
-                <div className="mb-8">
-                    <div>
-                        <span className="text-gray-500">Bio: </span>
-                        <p className="whitespace-pre-line">{user?.bio}</p>
-                    </div>
-                    <p>
-                        <span className="text-gray-500">Email: </span>
-                        <a
-                            href={`mailto:${user?.email}`}
-                            className="underline text-purple-400"
-                        >
-                            {user?.email}
-                        </a>
-                    </p>
-                    <p>
-                        <span className="text-gray-500">Website: </span>
-                        <a
-                            href={user?.website}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="underline text-purple-400"
-                        >
-                            {user?.website}
-                        </a>
-                    </p>
-                </div>
-                <div>
-                    <Button
-                        title="Edit Profile"
-                        looks="main"
-                        onClick={() => setModalStatus(!modalStatus)}
-                    />
-                </div>
+            {user !== null && (
+                <ProfileCard
+                    user={user}
+                    isModalOpen={imageModal}
+                    updateIsModalOpen={updateImageModal}
+                />
+            )}
+            <div>
+                <Button
+                    title="Edit Profile"
+                    looks="main"
+                    onClick={() => updateEditModal(!editModal)}
+                />
             </div>
-            {modalStatus && <EditProfile setModalStatus={setModalStatus} />}
-            {updateImageModal && (
-                <ChangeImageProfile setModalStatus={setUpdateImageModal} />
+            {editModal && <EditProfile setModalStatus={updateEditModal} />}
+            {imageModal && (
+                <ChangeImageProfile setModalStatus={updateImageModal} />
             )}
         </div>
     );
