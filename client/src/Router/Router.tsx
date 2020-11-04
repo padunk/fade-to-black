0762da644d741f9a707eca698eca13120be07437
Padunk <abraham.agel@gmail.com>
2020-11-04 12:09:22 +0700

    new page

--- client/src/Router/Router.tsx
+++ client/src/Router/Router.tsx
@@ -9,6 +9,7 @@ import Home from "../Pages/Home";
 import { RootState } from "types";
 import WhisperDetail from "../Pages/WhisperDetail";
 import UserProfile from "../Pages/UserProfile";
+import UserDetail from "../Pages/UserDetail";
 
 function PrivateRoute({ children, ...rest }: any) {
     return (
@@ -37,6 +38,11 @@ const Router: React.FC<{ authenticated: boolean }> = ({ authenticated }) => {
                 <Switch>
                     <Route path="/" exact component={Home} />
                     <Route path="/about" exact component={About} />
+                    <Route
+                        path="/user/:userName"
+                        exact
+                        component={UserDetail}
+                    />
                     <PrivateRoute path="/feed" authenticated={authenticated}>
                         <Feed />
                     </PrivateRoute>
@@ -47,7 +53,7 @@ const Router: React.FC<{ authenticated: boolean }> = ({ authenticated }) => {
                         <WhisperDetail />
                     </PrivateRoute>
                     <PrivateRoute
-                        path="/user/:userName"
+                        path="/profile/:userName"
                         authenticated={authenticated}
                     >
                         <UserProfile />
