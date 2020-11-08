ba81e98c1838375f4dfbeda6600bbd03b493db42
Padunk <abraham.agel@gmail.com>
2020-11-08 09:13:08 +0700

    refactoring

--- client/src/Auth/components/Welcome.tsx
+++ client/src/Auth/components/Welcome.tsx
@@ -1,20 +1,35 @@
 import React from "react";
+import { UserCredentials } from "types";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
-const Welcome = ({ credentials }: any) => {
-    const [userName, setUserName] = React.useState<null | string>(null);
-    React.useEffect(() => {
-        if (credentials && credentials.userName) {
-            setUserName(credentials.userName);
-        }
-    }, [credentials]);
-    return (
-        <div>
-            <h2>Hi {userName !== null ? userName : ""}!</h2>
-            <p>
-                You have been logged in for: <span>5 Mins</span>
-            </p>
-        </div>
-    );
+dayjs.extend(relativeTime);
+
+type IWelcomeProps = {
+    credentials: UserCredentials | null;
+};
+
+const Welcome: React.FC<IWelcomeProps> = ({ credentials }) => {
+    const showWelcome = () =>
+        credentials !== null ? (
+            <div className="flex flex-col justify-center">
+                <h2 className="text-4xl text-orange-500">
+                    Hi {credentials.userName}!
+                </h2>
+                <p>You have been logged in for: </p>
+                <strong className="block text-4xl text-purple-600">
+                    {dayjs().from(dayjs(credentials.lastLogin), true)}
+                </strong>
+            </div>
+        ) : (
+            <div>
+                <h2 className="text-4xl text-orange-500">
+                    Welcome to Our Stories!
+                </h2>
+            </div>
+        );
+
+    return showWelcome();
 };
 
 export default Welcome;
