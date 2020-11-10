032770ffc974dd3a57a73d57aaa2888d78133d2f
Padunk <abraham.agel@gmail.com>
2020-11-10 12:49:30 +0700

    loading...

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -1,34 +1,48 @@
 import React from "react";
 import { gsap } from "gsap";
 import Container from "../Container/Container";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(TextPlugin);
 
 const Loading = () => {
-    React.useEffect(() => {
-        const tl = gsap.timeline({
-            repeat: Infinity,
-            repeatDelay: 1,
-            ease: "power2",
-            yoyo: true,
-        });
+    const timeline = gsap.timeline({
+        repeat: Infinity,
+        repeatDelay: 1,
+        ease: "power2",
+        yoyo: true,
+    });
+    const { current: tl } = React.useRef(timeline);
+    const loadingEl = React.useRef(null);
 
-        tl.to(".loading", {
-            duration: 2,
+    React.useEffect(() => {
+        tl.to(loadingEl.current, {
+            duration: 1,
             text: {
-                value: "Our Whispers",
+                value: "",
             },
         })
-            .to(".loading", {
+            .to(loadingEl.current, {
                 duration: 1,
-                text: "",
+                text: {
+                    value: "Made by:",
+                    newClass: "text-purple-600",
+                },
             })
-            .to(".loading", {
-                duration: 2,
-                text: "Made by Abraham Anak Agung",
+            .to(loadingEl.current, {
+                duration: 1,
+                text: {
+                    value:
+                        "<a href='https://anakagung.com' class='underline'>Abraham Anak Agung</a>",
+                    newClass: "text-purple-600",
+                },
             });
-    });
+    }, [loadingEl, tl]);
     return (
         <Container>
-            <h2 className="loading text-orange-500"></h2>
+            <h2 ref={loadingEl} className="text-orange-500 text-2xl">
+                Our Whispers
+            </h2>
         </Container>
     );
 };
